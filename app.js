dbc0c53083f06166c20ce2a66ce2ab94bf07559f
baoquitran <baoquitran411@gmail.com>
2020-11-07 16:53:00 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// nav bar responvive
+document.querySelector('.menu-bar').addEventListener('click', () =>
+document.querySelector('.main-nav').classList.toggle('show'));
+
+//burger icon
+function burgeranimation(x){
+  x.classList.toggle('change');
+}
